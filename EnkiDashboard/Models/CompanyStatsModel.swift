87114624667fe73618f5121c90e43a9781968780
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2020-12-26 20:57:40 +0300

    Create CompanyStatsModel

---
+++ EnkiDashboard/Models/CompanyStatsModel.swift
@@ -0,0 +1,24 @@
+//
+//  CompanyStatsModel.swift
+//  EnkiDashboard
+//
+//  Created by Ebubekir Tabak on 21.11.2020.
+//
+
+import Foundation
+
+struct CompanyStatsModel {
+    let companyName: String
+    let finishedReports: Int32
+    let waitingReports: Int32
+    let topReports: Int32
+}
+
+extension CompanyStatsModel: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case companyName = "company_name"
+        case finishedReports = "finished_reports"
+        case waitingReports = "waiting_reports"
+        case topReports = "top_reports"
+    }
+}
