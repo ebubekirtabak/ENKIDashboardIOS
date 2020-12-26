e5bda4ed22042ee46fba370fc2bfb49328ca440e
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2020-12-26 20:59:04 +0300

    Create HttpService.

---
+++ EnkiDashboard/Services/HttpService.swift
@@ -0,0 +1,41 @@
+//
+//  HttpService.swift
+//  EnkiDashboard
+//
+//  Created by Ebubekir Tabak on 15.11.2020.
+//
+
+import Foundation
+
+class HttpService {
+    var apiURL: String
+    
+    init() {
+        self.apiURL = "http://169.63.92.54:3001/v1/"
+    }
+    
+    func postRequest() {
+        struct Response: Codable {
+            let status: String?
+            let error: String?
+        }
+    }
+    
+    func loadJson(fromURLString urlString: String,
+                          completion: @escaping (Result<Data, Error>) -> Void) {
+        if let url = URL(string: urlString) {
+            let urlSession = URLSession(configuration: .default).dataTask(with: url) { (data, response, error) in
+                if let error = error {
+                    completion(.failure(error))
+                }
+                
+                if let data = data {
+                    completion(.success(data))
+                }
+            }
+            
+            urlSession.resume()
+        }
+    }
+}
+
