351a2b9d1d700e7790b4005287b8099aae6901ef
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2020-12-26 21:09:27 +0300

    Create CardVluesView.

---
+++ EnkiDashboard/Views/ReportCardView/CardValuesView.swift
@@ -0,0 +1,46 @@
+//
+//  CardValuesView.swift
+//  EnkiDashboard
+//
+//  Created by Ebubekir Tabak on 10.12.2020.
+//
+
+import SwiftUI
+
+struct CardValuesView: View {
+    @Binding var cardValues: [CardValuesModel]
+    
+    var body: some View {
+        LazyVGrid (columns: [
+            GridItem(.adaptive(minimum: 70)),
+            GridItem(.adaptive(minimum: 70))
+        ], content: {
+            CardValueView(cardValue: self.$cardValues[0])
+            CardValueView(cardValue: self.$cardValues[1])
+            CardValueView(cardValue: self.$cardValues[2])
+            CardValueView(cardValue: self.$cardValues[3])
+        })
+        .frame(maxWidth: .infinity)
+    }
+}
+
+struct CardValuesView_Previews: PreviewProvider {
+    @State static var cardValues: [CardValuesModel] = [
+        CardValuesModel(
+            iconName: "checkmark", backgroundColorName: "SecondaryAccentColor", cardValue: "211,961"
+        ),
+        CardValuesModel(
+            iconName: "checkmark", backgroundColorName: "SecondaryAccentColor", cardValue: "578"
+        ),
+        CardValuesModel(
+            iconName: "checkmark", backgroundColorName: "SecondaryAccentColor", cardValue: "0"
+        ),
+        CardValuesModel(
+            iconName: "checkmark", backgroundColorName: "SecondaryAccentColor", cardValue: "159,756"
+        )
+    ]
+    
+    static var previews: some View {
+        CardValuesView(cardValues: $cardValues)
+    }
+}
