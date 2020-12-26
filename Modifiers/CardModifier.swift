5397fcf69256cbc1ec8c61627bcbafdd41c7d27a
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2020-12-26 21:12:20 +0300

    Create CardModifier.

---
+++ Modifiers/CardModifier.swift
@@ -0,0 +1,20 @@
+//
+//  CardModifier.swift
+//  EnkiDashboard
+//
+//  Created by Ebubekir Tabak on 1.12.2020.
+//
+
+import SwiftUI
+
+struct CardModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardModifier_Previews: PreviewProvider {
+    static var previews: some View {
+        CardModifier()
+    }
+}
