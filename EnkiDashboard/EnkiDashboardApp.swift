54840f96df7ed6fc51fd2863627d9752af1419a6
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2020-12-26 21:11:41 +0300

    Create EnkiDashboardApp.

---
+++ EnkiDashboard/EnkiDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnkiDashboardApp.swift
+//  EnkiDashboard
+//
+//  Created by Ebubekir Tabak on 3.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct EnkiDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
