397d38218f846edcf3576cf8839143797db5cfcf
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2020-12-26 21:08:58 +0300

    Create CompanyListView.

---
+++ EnkiDashboard/CompanyListView.swift
@@ -0,0 +1,20 @@
+//
+//  CompanyListView.swift
+//  EnkiDashboard
+//
+//  Created by Ebubekir Tabak on 19.11.2020.
+//
+
+import SwiftUI
+
+struct CompanyListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CompanyListView_Previews: PreviewProvider {
+    static var previews: some View {
+        CompanyListView()
+    }
+}
