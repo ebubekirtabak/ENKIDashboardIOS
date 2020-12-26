8e72cd919150ddef978cd7ace0d53b0017a64518
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2020-12-26 20:56:19 +0300

    Create CardValuesModel

---
+++ EnkiDashboard/Models/ReportCardModels/CardValuesModel.swift
@@ -0,0 +1,14 @@
+//
+//  CardValuesModel.swift
+//  EnkiDashboard
+//
+//  Created by Ebubekir Tabak on 12.12.2020.
+//
+
+import Foundation
+
+struct CardValuesModel: Codable {
+    let iconName: String
+    let backgroundColorName: String
+    let cardValue: String
+}
