5b4d5a2d00cef1babb4f6075e69d1631781d883a
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2021-01-07 00:36:38 +0300

    Processbarview filled.

--- EnkiDashboard/Views/ReportCardView/ReportCardView.swift
+++ EnkiDashboard/Views/ReportCardView/ReportCardView.swift
@@ -38,13 +38,13 @@ struct ReportCardView: View {
                         .foregroundColor(Color("PrimaryDarkColor"))
                         .lineLimit(2)
                         .padding(.bottom, 1.0)
-      
+                    
                     
                     Text(String(self.cardData.currentValue))
                         .font(.system(size: 20))
                         .fontWeight(.heavy)
                         .foregroundColor(Color("PrimaryDarkColor"))
-                    ProgressBarView(value: .constant(0.37))
+                    ProgressBarView(value: .constant(self.getProcessRate(cardData: self.cardData)))
                 }.padding(.all, 10.0)
                 CardValuesView(cardValues: Binding(get: { self.cardData.cardValues }, set: { _,_ in self.cardData.cardValues = [] }))
                     .padding(EdgeInsets(.init(top: 0, leading: 10, bottom: 10, trailing: 10)))
@@ -53,6 +53,25 @@ struct ReportCardView: View {
         .frame(maxWidth: .infinity, alignment: .trailing)
         .background(Color.white)
         .modifier(CardModifier())
+    }
+    
+    func divide(a : Int32, b : Int32) -> Float {
+      if b == 0 { return 0 }
+      return Float(a) / Float(b)
+    }
+    
+    func getProcessRate(cardData: ReportCardDataModel) -> Float {
+        var diff: Int32
+        var percent: Float
+        if (cardData.currentValue > cardData.waitingValue) {
+            diff = cardData.currentValue - cardData.waitingValue
+            percent = 100 * self.divide(a: diff, b: cardData.currentValue)
+            return percent
+        }
+        
+        diff = cardData.waitingValue - cardData.currentValue;
+        percent = self.divide(a: diff, b: cardData.waitingValue)
+        return 1.0 - percent
     }
 }
 
@@ -68,19 +87,25 @@ struct ReportCardView_Previews: PreviewProvider {
             title: "Done", iconName: "", backgroundColorName: "", cardValue: 0
         ),
         CardValuesModel(
-            title: "Done", iconName: "", backgroundColorName: "", cardValue: 159756
+            title: "Done", iconName: "", backgroundColorName: "", cardValue:  159756
         )
     ]
     
     @State static var cardData = ReportCardDataModel(
+        title: "",
         imageUrl: "http://52.116.101.196/static/media/linkedin_logo.c24cde28.png",
-        currentValue: 2756,
+        balloonColor: "SecondaryAccentColor",
+        currentValue: 126,
+        waitingValue: 1342,
         cardValues: cardValues
     )
     
     @State static var googleCardData = ReportCardDataModel(
+        title: "",
         imageUrl: "http://169.63.92.52:5000/static/company_images/google.png",
+        balloonColor: "AlertColor",
         currentValue: 1066,
+        waitingValue: 1342,
         cardValues: cardValues
     )
     static var previews: some View {
